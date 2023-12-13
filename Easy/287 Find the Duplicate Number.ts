a0eb9af11176a69aed83194156933871155b320b
Nayan Bagale <nvbagale@gmail.com>
2023-12-13 22:18:04 +0530

    Accepted Code

--- Easy/287 Find the Duplicate Number.ts
+++ Easy/287 Find the Duplicate Number.ts
@@ -4,7 +4,7 @@ const swap = (first: number, second: number, array: number[]): void => {
   array[second] = temp;
 };
 
-const missingNumber = (nums: number[]): number => {
+const findDuplicate = (nums: number[]): number => {
   let i = 0;
   while (i < nums.length) {
     let current = nums[i] - 1;
@@ -18,12 +18,12 @@ const missingNumber = (nums: number[]): number => {
   return nums[i - 1];
 };
 
-console.log(missingNumber([1, 3, 4, 2, 2])); //2
-console.log(missingNumber([3, 1, 3, 4, 2])); //3
-console.log(missingNumber([2, 1, 2])); //2
-console.log(missingNumber([4, 3, 1, 4, 2])); //4
+console.log(findDuplicate([1, 3, 4, 2, 2])); //2
+console.log(findDuplicate([3, 1, 3, 4, 2])); //3
+console.log(findDuplicate([2, 1, 2])); //2
+console.log(findDuplicate([4, 3, 1, 4, 2])); //4
 console.log(
-  missingNumber([
+  findDuplicate([
     8, 7, 1, 10, 17, 15, 18, 11, 16, 9, 19, 12, 5, 14, 3, 4, 2, 13, 18, 18,
   ])
 ); //4
