0ceea436ea2ff2919ace28f250cee9103b18715d
Nayan Bagale <nvbagale@gmail.com>
2023-12-31 17:38:02 +0530

    Accepted Code

--- Hard/51 N-Queens.ts
+++ Hard/51 N-Queens.ts
@@ -1,25 +1,39 @@
-// #TODO:Complete this Code
+const isSafe = (board: boolean[][], row: number, col: number): boolean => {
+  //vertical
+  for (let i = row; i >= 0; i--) if (board[row - i][col]) return false;
+  
+  //diagonal left
+  let minLeft = Math.min(row, col);
+  for (let i = minLeft; i >= 0; i--) if (board[row - i][col - i]) return false;
 
-function solveNQueens(n: number): any {
-  const board = new Array(n).fill(new Array(n).fill(false));
-  return helper(board, 0, 0);
-}
+  //diagonal right
+  let minRight = Math.min(row, board[0].length - col + 1);
+  for (let i = 0; i <= minRight; i++) if (board[row - i][col + i]) return false;
 
-const helper = (board: boolean[][], row: number, col: number): any => {
-    if(row === board.length){
-        console.log(row);
-    }
-
-    if(col < board.length){
-        helper(board,row,col++)
-    }
-
-    if(row < board.length){
-        helper(board,row++,col)
-    }
-
-
+  return true;
 };
 
-console.log(solveNQueens(3));
-//[q...,..q.]
+const helper = (board: boolean[][], row: number, ans: string[][]): string[][] => {
+  if (row === board.length) {
+    let list = board.map((row) => row.map((col) => (col ? "Q" : ".")).join(""));
+    ans.push(list);
+    return ans;
+  }
+
+  for (let col = 0; col < board[0].length; col++) {
+    if (isSafe(board, row, col)) {
+      board[row][col] = true;
+      helper(board, row + 1, ans);
+      board[row][col] = false;
+    }
+  }
+
+  return ans;
+};
+
+function solveNQueens(n: number): string[][] {
+  const board = Array.from(Array(n), () => new Array(n).fill(false));
+  return helper(board, 0, []);
+}
+
+console.log(solveNQueens(4));
