9b225320bedf0249075919989bc99c7da3b76a34
Nayan Bagale <nvbagale@gmail.com>
2024-01-12 21:30:44 +0530

    Accepted but not efficient.

--- Medium/90 Subsets II.ts
+++ Medium/90 Subsets II.ts
@@ -1,26 +1,30 @@
 //TODO: Complete the code.
 
 function subsetsWithDup(nums: number[]): any {
-    let ans:number[][] = [];
-    let stack:number[] = [];
-    nums.sort((a,b) => a - b);
-    helper(stack,nums,0)
-}
-const helper = (stack:number[],nums:number[], count:number) => {
-    if(nums.length === count ){
-        // console.log(stack)
-        return stack
+  let res: number[][] = [];
+  let subset: number[] = [];
+
+  nums.sort((a, b) => a - b);
+
+  (function dfs(i: number) {
+    if (i >= nums.length) {
+        let bool = res.some((x) => x.toString() === subset.toString());
+        if (bool) return;
+        let subsetSum = subset.reduce((a, b) => a + b, 0);
+      if (subsetSum !== 0 && bool) return;
+      res.push([...subset]);
+      return;
     }
 
-    for(let i = 0 ; i<=count; i++){
-        console.log(i,count)
-        if (i > 0 && nums[i - 1] === nums[i]) continue;
-        stack.push(nums[i]);
-        console.log(stack)
-        helper(stack,nums,count+1)
-        stack.pop()
-    }
+    subset.push(nums[i]);
+    dfs(i + 1);
+    subset.pop();
+    dfs(i + 1);
+  })(0);
+
+  return res;
 }
 
 console.log(subsetsWithDup([1, 2, 2]));
-// console.log(subsetsWithDup([0]));
+console.log(subsetsWithDup([0]));
+console.log(subsetsWithDup([4, 4, 4, 1, 4]));
