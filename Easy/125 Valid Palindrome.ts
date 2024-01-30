2911bb8737230f4717b6d18824a9091afe2ba6ce
Nayan Bagale <nvbagale@gmail.com>
2024-01-30 14:52:07 +0530

    Code Accepted

--- Easy/125 Valid Palindrome.ts
+++ Easy/125 Valid Palindrome.ts
@@ -1,18 +1,23 @@
 function isPalindrome(s: string) {
-    let newS = s.toLowerCase().split('').filter((e) => {
-        if ((e.charCodeAt(0) >= 'a'.charCodeAt(0) && e.charCodeAt(0) <= 'z'.charCodeAt(0) || (e.charCodeAt(0) >= '0'.charCodeAt(0) && e.charCodeAt(0) <= '9'.charCodeAt(0))))
-            return e;
-
-    })
+    let newS = s
+        .toLowerCase()
+        .split('')
+        .filter((e) => {
+            if (
+                (e.charCodeAt(0) >= 'a'.charCodeAt(0) &&
+                    e.charCodeAt(0) <= 'z'.charCodeAt(0)) ||
+                (e.charCodeAt(0) >= '0'.charCodeAt(0) &&
+                    e.charCodeAt(0) <= '9'.charCodeAt(0))
+            )
+                return e
+        })
 
     for (let i = 0, j = newS.length - 1; i < j; i++, j--)
-        if (newS[i].toLowerCase() !== newS[j].toLowerCase())
-            return false
-
-
+        if (newS[i].toLowerCase() !== newS[j].toLowerCase()) return false
 
     return true
-};
+}
+
 
 console.log(isPalindrome("A man, a plan, a canal: Panama"));
 console.log(isPalindrome("race a car"));
