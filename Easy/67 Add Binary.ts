86c0c8c883ee12cb2e05b082d6028bd662f6fe22
Nayan Bagale <nvbagale@gmail.com>
2024-02-02 23:07:55 +0530

    Not yet completed.

---
+++ Easy/67 Add Binary.ts
@@ -0,0 +1,7 @@
+function addBinary(a: string, b: string): string {
+    console.log(parseInt(a) | parseInt(b))
+    return ''
+};
+
+console.log(addBinary('11', '1'))
+console.log(addBinary('1010', '1011'))
